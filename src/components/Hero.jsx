7b1f92bdb4de0055746abb0cdc6238ec8f7305b8
Joynal Bokhsho <shaownsilva@gmail.com>
2025-05-02 00:58:15 +0600

    Update example format for student ID in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = ({ studentId, setStudentId, semesterId, setSemesterId, onSubmit }) 
                       required
                     />
                   </div>
-                  <div className="form-text">Example: 251-27-014</div>
+                  <div className="form-text">Example: ***-**-***</div>
                 </div>
                 
                 <div className="mb-4">
